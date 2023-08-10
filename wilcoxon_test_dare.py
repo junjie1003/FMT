9a4d77da12909c2668e00f9243d0fa85f24e6222
junjie1003 <jjyang@tju.edu.cn>
2023-08-10 15:19:26 +0800

    update wilcoxon test

--- wilcoxon_test_dare.py
+++ wilcoxon_test_dare.py
@@ -1,109 +1,81 @@
+import io
+import sys
 import argparse
-import numpy as np
+import warnings
 import pandas as pd
 from scipy.stats import wilcoxon
-from itertools import combinations
-from openpyxl import load_workbook
+
+warnings.filterwarnings("ignore")
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description="Test")
-    parser.add_argument("--sheet-name", default="Sheet1", type=str, help="Sheet name")
-    parser.add_argument("--table-range", type=str, help="Table range")
+    parser = argparse.ArgumentParser(description="Wilcoxon signed-rank test")
+    parser.add_argument("--testwith", type=str, choices=["CW", "FGSM", "JSMA", "PGD"])
+    parser.add_argument("--model", type=str, choices=["VGG16", "VGG19", "Alexnet"])
+    parser.add_argument("--dataset", type=str, choices=["CIFAR10", "SVHN", "FM"])
     args = parser.parse_args()
 
     return args
 
 
-# # NNrepair
-# nnrepair = {"intermediate": [87.25, 87.3, 34.61, 81.04, 80.93, 36.3], "last": [87.25, 87, 34.76, 81.04, 80.77, 36.23]}
-# dare = [88.5, 88.87, 66.43, 81.3, 81.87, 58.7]
-
-# for layer in ["intermediate", "last"]:
-#     print(f"Dare v.s. NNrepair-{layer}")
-#     base = nnrepair[layer]
-#     w, p = wilcoxon(dare, base, alternative="greater")
-
-#     print("Wilcoxon Test统计量:", w)
-#     print("单侧P值:", p)
-
-#     alpha = 0.05
-
-#     if p < alpha:
-#         print("拒绝原假设，您的方法的偏差显著小于基线方法的偏差。")
-#     else:
-#         print("不能拒绝原假设，您的方法的偏差与基线方法的偏差无显著差异。")
+f = open("./log/wilcoxon_test_dare.log", "a", encoding="UTF-8")
+sys.stdout = f
+sys.stderr = f
 
 args = get_args()
+print(f"Model: {args.model}\nDataset: {args.dataset}\nTestwith: {args.testwith}\n")
 
-# 读取 Excel 文件
-file_path = "dare.xlsx"
-wb = load_workbook(file_path)
+path = "./data/dare.xlsx"
+approaches = ["Dare", "CW", "FGSM", "JSMA", "PGD"]
 
-sheet_name = args.sheet_name
-ws = wb[sheet_name]
+if args.testwith == "CW":
+    col_range = "C:G"
+elif args.testwith == "FGSM":
+    col_range = "H:L"
+elif args.testwith == "JSMA":
+    col_range = "M:Q"
+elif args.testwith == "PGD":
+    col_range = "R:V"
 
-# 定义各个表格的范围
-# table_ranges = ["C2:H11", "I2:N11", "O2:T11", "U2:Z11", "AA2:AF11"]
-# table_ranges = ["C34:D43", "E34:F43", "G34:H43", "I34:J43"]
-# table_ranges = ["C47:F56", "G47:J56", "K47:N56", "O47:R56", "S47:V56"]
-table_ranges = ["C60:D69", "E60:F69", "G60:H69", "I60:J69", "K60:L69"]
+if args.model == "VGG16":
+    base_row = 3
+elif args.model == "VGG19":
+    base_row = 30
+elif args.model == "Alexnet":
+    base_row = 57
 
+if args.dataset == "CIFAR10":
+    skip_row = base_row
+elif args.dataset == "SVHN":
+    skip_row = base_row + 9
+elif args.dataset == "FM":
+    skip_row = base_row + 18
 
-# 使用 pandas 读取每个表格并将其存储在字典中
-tables = {}
-for index, table_range in enumerate(table_ranges):
-    data = ws[table_range]
-    rows = list(data)
+df = pd.read_excel(path, sheet_name=0, header=None, usecols=col_range, skiprows=skip_row, nrows=9)
+df.columns = approaches
+print(df)
 
-    # 将表格数据转换为列表
-    table_data = [[cell.value for cell in row] for row in rows]
+approaches.remove("Dare")
 
-    # 转换为 pandas DataFrame
-    df = pd.DataFrame(table_data[1:], columns=table_data[0])
-    df.replace("-", np.nan, inplace=True)
-    df = df.fillna(0)
-    tables[index] = df
-
-merged_df = pd.concat([tables[0], tables[1], tables[2], tables[3], tables[4]], axis=0)
-# merged_df = pd.concat([tables[0], tables[1], tables[2], tables[3]], axis=0)
-merged_df.reset_index(drop=True, inplace=True)
-print(merged_df)
-column_names = list(df.columns)
-
-# data = ws[args.table_range]
-# rows = list(data)
-
-# # 将表格数据转换为列表
-# table_data = [[cell.value for cell in row] for row in rows]
-
-# # 转换为 pandas DataFrame
-# df = pd.DataFrame(table_data[1:], columns=table_data[0])
-# column_names = list(df.columns)
-# print(column_names)
-# print(df)
-
-baselines = {}
-for col in column_names:
-    # base = df[col].to_numpy()
-    base = merged_df[col].to_numpy()
-    baselines[col] = base
-print(baselines)
-
-dare = baselines["Dare"]
-column_names.remove("Dare")
-
-for col in column_names:
-    print(f"Dare v.s. {col}")
-    base = baselines[col]
-    w, p = wilcoxon(dare, base, alternative="greater")
-
-    print("Wilcoxon Test统计量:", w)
-    print("单侧P值:", p)
-
+for app in approaches:
+    print(f"\nDare v.s. {app}\n")
+    w1, p1 = wilcoxon(df["Dare"].to_list(), df[app].to_list(), alternative="greater")
+    print("Wilcoxon Test统计量:", w1)
+    print("单侧P值:", p1)
     alpha = 0.05
-
-    if p < alpha:
+    if p1 < alpha:
         print("拒绝原假设，您的方法的偏差显著小于基线方法的偏差。")
     else:
         print("不能拒绝原假设，您的方法的偏差与基线方法的偏差无显著差异。")
+
+    print(f"\n{app} v.s. Dare\n")
+    w2, p2 = wilcoxon(df[app].to_list(), df["Dare"].to_list(), alternative="greater")
+    print("Wilcoxon Test统计量:", w2)
+    print("单侧P值:", p2)
+    alpha = 0.05
+    if p2 < alpha:
+        print("拒绝原假设，您的方法的偏差显著小于基线方法的偏差。")
+    else:
+        print("不能拒绝原假设，您的方法的偏差与基线方法的偏差无显著差异。")
+
+print("-" * 120)
--- wilcoxon_test_fmt.py
+++ wilcoxon_test_fmt.py
@@ -1,17 +1,6 @@
-import argparse
 import numpy as np
 import pandas as pd
 from scipy.stats import wilcoxon
-from itertools import combinations
-from openpyxl import load_workbook
-
-
-def get_args():
-    parser = argparse.ArgumentParser(description="Test")
-    parser.add_argument("--table-range", type=str, help="Table range")
-    args = parser.parse_args()
-
-    return args
 
 
 def get_metric_results(file_path, metric):
@@ -25,34 +14,83 @@ def get_metric_results(file_path, metric):
 
 
 # metric = "accuracy"
+# metric = "spd"
+# metric = "aaod"
 metric = "eod"
 augmentation = ["single", "dual"]
-approaches = ["FMT", "REW", "ADV", "ROC", "CARE"]
+approaches = ["FMT", "REW", "ADV", "ROC", "CARE", "SUPP"]
 tasks = ["adult_race", "adult_sex", "bank_age", "compas_race", "compas_sex", "german_age", "german_sex"]
 
+# # All subjects
+# result_dict = {}
+# for app in approaches:
+#     result_dict[app] = {}
+#     for aug in augmentation:
+#         result = []
+#         for task in tasks:
+#             if app == "FMT":
+#                 file_path = f"./RQ5_results/{app.lower()}/{app.lower()}_{aug}_{task}.txt"
+#             elif app == "SUPP":
+#                 file_path = f"./Major/suppression/suppression_{task}.txt"
+#             else:
+#                 file_path = f"./RQ5_results/{app.lower()}/{app.lower()}_{task}.txt"
+
+#             temp = get_metric_results(file_path, metric)
+#             result.extend(temp)
+#         result_dict[app][aug] = result
+
+# for app in ["REW", "ADV", "ROC", "CARE", "SUPP"]:
+#     for aug in augmentation:
+#         print(f"Ours v.s. {app} ({aug})")
+#         w, p = wilcoxon(result_dict["FMT"][aug], result_dict[app][aug], alternative="greater")
+#         # w, p = wilcoxon(result_dict["FMT"][aug], result_dict[app][aug], alternative="less")
+#         print("Wilcoxon Test统计量:", w)
+#         print("单侧P值:", p)
+#         alpha = 0.05
+#         if p < alpha:
+#             print("拒绝原假设，您的方法的偏差显著小于基线方法的偏差。")
+#         else:
+#             print("不能拒绝原假设，您的方法的偏差与基线方法的偏差无显著差异。")
+
+# Each subject
 result_dict = {}
 for app in approaches:
     result_dict[app] = {}
-    for aug in augmentation:
-        result = []
-        for task in tasks:
+    for task in tasks:
+        result_dict[app][task] = {}
+        for aug in augmentation:
+            result = []
             if app == "FMT":
                 file_path = f"./RQ5_results/{app.lower()}/{app.lower()}_{aug}_{task}.txt"
+            elif app == "SUPP":
+                file_path = f"./Major/suppression/suppression_{task}.txt"
             else:
                 file_path = f"./RQ5_results/{app.lower()}/{app.lower()}_{task}.txt"
 
             temp = get_metric_results(file_path, metric)
             result.extend(temp)
-        result_dict[app][aug] = result
+            result_dict[app][task][aug] = result
 
-for app in ["REW", "ADV", "ROC", "CARE"]:
-    for aug in augmentation:
-        print(f"Ours v.s. {app} ({aug})")
-        w, p = wilcoxon(result_dict["FMT"][aug], result_dict[app][aug], alternative="less")
-        print("Wilcoxon Test统计量:", w)
-        print("单侧P值:", p)
-        alpha = 0.05
-        if p < alpha:
-            print("拒绝原假设，您的方法的偏差显著小于基线方法的偏差。")
-        else:
-            print("不能拒绝原假设，您的方法的偏差与基线方法的偏差无显著差异。")
+for app in ["REW", "ADV", "ROC", "CARE", "SUPP"]:
+    for task in tasks:
+        for aug in augmentation:
+            print(f"Ours v.s. {app} ({task} {aug})")
+            # w1, p1 = wilcoxon(result_dict["FMT"][task][aug], result_dict[app][task][aug], alternative="greater")
+            w1, p1 = wilcoxon(result_dict["FMT"][task][aug], result_dict[app][task][aug], alternative="less")
+            print("Wilcoxon Test统计量:", w1)
+            print("单侧P值:", p1)
+            alpha = 0.05
+            if p1 < alpha:
+                print("拒绝原假设，您的方法的偏差显著小于基线方法的偏差。")
+            else:
+                print("不能拒绝原假设，您的方法的偏差与基线方法的偏差无显著差异。")
+
+            # w2, p2 = wilcoxon(result_dict[app][task][aug], result_dict["FMT"][task][aug], alternative="greater")
+            w2, p2 = wilcoxon(result_dict[app][task][aug], result_dict["FMT"][task][aug], alternative="less")
+            print("Wilcoxon Test统计量:", w2)
+            print("单侧P值:", p2)
+            alpha = 0.05
+            if p2 < alpha:
+                print("拒绝原假设，您的方法的偏差显著小于基线方法的偏差。")
+            else:
+                print("不能拒绝原假设，您的方法的偏差与基线方法的偏差无显著差异。")
