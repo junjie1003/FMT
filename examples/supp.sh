d48d9bc42b102ec5dd05867ed8d09f13a5f526d7
junjie1003 <jjyang@tju.edu.cn>
2023-08-10 15:16:40 +0800

    update logs

---
+++ examples/supp.sh
@@ -0,0 +1,10 @@
+python supp.py -d adult -p race
+python supp.py -d adult -p sex
+
+python supp.py -d bank -p age
+
+python supp.py -d compas -p race
+python supp.py -d compas -p sex
+
+python supp.py -d german -p age
+python supp.py -d german -p sex
---
+++ examples/wilcoxon_test_dare.sh
@@ -0,0 +1,44 @@
+python wilcoxon_test_dare.py --model VGG16 --dataset CIFAR10 --testwith CW
+python wilcoxon_test_dare.py --model VGG16 --dataset CIFAR10 --testwith FGSM
+python wilcoxon_test_dare.py --model VGG16 --dataset CIFAR10 --testwith JSMA
+python wilcoxon_test_dare.py --model VGG16 --dataset CIFAR10 --testwith PGD
+
+python wilcoxon_test_dare.py --model VGG16 --dataset SVHN --testwith CW
+python wilcoxon_test_dare.py --model VGG16 --dataset SVHN --testwith FGSM
+python wilcoxon_test_dare.py --model VGG16 --dataset SVHN --testwith JSMA
+python wilcoxon_test_dare.py --model VGG16 --dataset SVHN --testwith PGD
+
+python wilcoxon_test_dare.py --model VGG16 --dataset FM --testwith CW
+python wilcoxon_test_dare.py --model VGG16 --dataset FM --testwith FGSM
+python wilcoxon_test_dare.py --model VGG16 --dataset FM --testwith JSMA
+python wilcoxon_test_dare.py --model VGG16 --dataset FM --testwith PGD
+
+python wilcoxon_test_dare.py --model VGG19 --dataset CIFAR10 --testwith CW
+python wilcoxon_test_dare.py --model VGG19 --dataset CIFAR10 --testwith FGSM
+python wilcoxon_test_dare.py --model VGG19 --dataset CIFAR10 --testwith JSMA
+python wilcoxon_test_dare.py --model VGG19 --dataset CIFAR10 --testwith PGD
+
+python wilcoxon_test_dare.py --model VGG19 --dataset SVHN --testwith CW
+python wilcoxon_test_dare.py --model VGG19 --dataset SVHN --testwith FGSM
+python wilcoxon_test_dare.py --model VGG19 --dataset SVHN --testwith JSMA
+python wilcoxon_test_dare.py --model VGG19 --dataset SVHN --testwith PGD
+
+python wilcoxon_test_dare.py --model VGG19 --dataset FM --testwith CW
+python wilcoxon_test_dare.py --model VGG19 --dataset FM --testwith FGSM
+python wilcoxon_test_dare.py --model VGG19 --dataset FM --testwith JSMA
+python wilcoxon_test_dare.py --model VGG19 --dataset FM --testwith PGD
+
+python wilcoxon_test_dare.py --model Alexnet --dataset CIFAR10 --testwith CW
+python wilcoxon_test_dare.py --model Alexnet --dataset CIFAR10 --testwith FGSM
+python wilcoxon_test_dare.py --model Alexnet --dataset CIFAR10 --testwith JSMA
+python wilcoxon_test_dare.py --model Alexnet --dataset CIFAR10 --testwith PGD
+
+python wilcoxon_test_dare.py --model Alexnet --dataset SVHN --testwith CW
+python wilcoxon_test_dare.py --model Alexnet --dataset SVHN --testwith FGSM
+python wilcoxon_test_dare.py --model Alexnet --dataset SVHN --testwith JSMA
+python wilcoxon_test_dare.py --model Alexnet --dataset SVHN --testwith PGD
+
+python wilcoxon_test_dare.py --model Alexnet --dataset FM --testwith CW
+python wilcoxon_test_dare.py --model Alexnet --dataset FM --testwith FGSM
+python wilcoxon_test_dare.py --model Alexnet --dataset FM --testwith JSMA
+python wilcoxon_test_dare.py --model Alexnet --dataset FM --testwith PGD
